d07ade1d818333077a0b7dc50ddbe21c8485134c
sabaghhh <22-101257@students.eui.edu.eg>
2024-09-12 19:58:16 +0300

    done

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Task 0. User model
+
+An SQLAlchemy model named User
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User
+
+    A model to store users.
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
